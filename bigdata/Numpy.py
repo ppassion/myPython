273033f8371e46bf74e947ea6e05471d8d3a5ad4
ppassion <chenyanhui1087@163.com>
2021-02-28 14:01:02 +0800

    bigdata

---
+++ bigdata/Numpy.py
@@ -0,0 +1,5 @@
+# Coding : utf-8
+# Author : chyh
+# Date   : 2021/2/28 13:59
+
+import numpy
---
+++ study/io.py
@@ -0,0 +1,7 @@
+# Coding : utf-8
+# Author : chyh
+# Date   : 2021/2/27 18:34
+
+if __name__ == '__main__':
+    word = input("sss")
+    print("input is %s" % word)
