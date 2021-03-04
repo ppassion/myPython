e14cb680ca9bb7f81fb8243d4384be615ee56b40
ppassion <chenyanhui1087@163.com>
2021-03-04 23:34:26 +0800

    ai

--- bigdata/fit_transform.py
+++ bigdata/fit_transform.py
@@ -4,6 +4,7 @@
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
 
 
 # 转换器
@@ -24,5 +25,22 @@ def tfidf():
     print(data.toarray())
 
 
+def pca():
+    data = [[1, 2, 3, 4], [1, 1, 1, 1], [1, 3, 5, 7]]
+    # 1、实例化PCA, 小数——保留多少信息
+    transfer = PCA(n_components=0.9)
+    # 2、调用fit_transform
+    data1 = transfer.fit_transform(data)
+
+    print("保留90%的信息，降维结果为：\n", data1)
+
+    # 1、实例化PCA, 整数——指定降维到的维数
+    transfer2 = PCA(n_components=3)
+    # 2、调用fit_transform
+    data2 = transfer2.fit_transform(data)
+    print("降维到3维的结果：\n", data2)
+
+
 if __name__ == '__main__':
-    tfidf()
+    # tfidf()
+    pca()
