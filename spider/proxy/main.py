93b105ba961db21b7da0fb85c93ec57cfc115f41
ppassion <chenyanhui1087@163.com>
2021-03-08 21:55:34 +0800

    spider

---
+++ spider/proxy/main.py
@@ -0,0 +1,8 @@
+# Coding : utf-8
+# Author : chyh
+# Date   : 2021/3/8 20:37
+
+from src.runner import run
+
+if __name__ == '__main__':
+    run()
---
+++ spider/proxy/src/runner.py
@@ -0,0 +1,17 @@
+# Coding : utf-8
+# Author : chyh
+# Date   : 2021/3/8 20:37
+
+from spider.proxy.src.log.logger import logger
+
+
+def run():
+    logger.info('初始化sqlite数据库...')
+    # sqlite_opt.init_db()
+    # scheduler = BackgroundScheduler()
+    # scheduler.add_job(crawl, 'interval', seconds=SPIDER['crawl_interval'])
+    # scheduler.add_job(validator.run, 'interval', seconds=VALIDATOR['validate_interval'])
+    # scheduler.add_job(anonymity_validator.run, 'interval', seconds=ANONYMITY_VALIDATOR['interval'])
+    # scheduler.add_job(expiration_validator.run, 'interval', seconds=EXPIRATION_VALIDATOR['interval'])
+    # scheduler.start()
+    # app.run(host=WEB_SERVER['host'], port=WEB_SERVER['port'])
