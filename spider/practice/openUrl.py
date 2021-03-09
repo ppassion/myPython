3eb10fd452314dd3c212734c8ab3a708cefa3527
ppassion <chenyanhui1087@163.com>
2021-03-09 22:17:25 +0800

    spider

--- spider/practice/openUrl.py
+++ spider/practice/openUrl.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
     configFile = open("urlInfo.yml", 'r', encoding='utf-8')
     config = configFile.read()
     configMap = yaml.load(config, Loader=yaml.FullLoader)
-    posts = "98849  98880  98842  98857  98859  98870  94591  94613  94604  88694  " \
-            "88709  88710  88706  14025  45342  67665  67658  59203  59171  52267  " \
-            "57507  57363  45275  52262  52428  52283  45303 "
+    posts = '''
+    104404  102951  102961  103957  102909  98031  101000
+104824  104658  102935  102905  102946  98102
+    '''
     array = posts.split()
     print(array)
     for i in array:
---
+++ spider/proxy/src/db/db_opt_interface.py
@@ -0,0 +1,8 @@
+# Coding : utf-8
+# Author : chyh
+# Date   : 2021/3/9 22:12
+
+class DataBaseOptInterface(object):
+
+    def add_proxy(self, proxy):
+        raise NotImplementedError
