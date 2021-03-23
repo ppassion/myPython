0fea694b1aaa45ac7728835a6919d360fd2d2db3
ppassion <chenyanhui1087@163.com>
2021-03-23 19:57:44 +0800

    proxy

--- spider/proxy/src/proxySpider/i_proxy_spider.py
+++ spider/proxy/src/proxySpider/i_proxy_spider.py
@@ -3,5 +3,13 @@
 # Date   : 2021/3/10 23:44
 
 class i_proxy_spider(object):
+
+    def __init__(self,name='unknown'):
+        self._name = name
+        self._urls = self.get_urls()
+
+    def get_urls(self) -> list[str]:
+        raise NotImplementedError
+
     def crawl(self):
         print(11)
