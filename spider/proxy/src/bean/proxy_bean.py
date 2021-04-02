8589e0613495018eb1cb1923ced3d66d7080491c
ppassion <chenyanhui1087@163.com>
2021-04-02 21:26:25 +0800

    proxy

--- spider/proxy/src/bean/proxy_bean.py
+++ spider/proxy/src/bean/proxy_bean.py
@@ -9,9 +9,38 @@ Base = declarative_base()
 
 
 class proxy_bean(Base):
+    '''
+                create table if not exists proxy(
+                ip varchar(20) not null,
+                port varchar(5) not null,
+                source varchar(50),
+                supplier varchar(32),
+                proxy_type tinyint(3),
+                proxy_cover tinyint(3),
+                check_count int(10),
+                region varchar(36),
+                last_check_time text,
+                create_time text default (datetime(CURRENT_TIMESTAMP,'localtime')),
+                reliability integer not null default 0 check(reliability >= 0) check(reliability <= 15),
+                PRIMARY KEY ("ip","port")
+                )
+
+                '''
     __tablename__ = 'proxy'
     ip = Column(String(20), primary_key=True)
     port = Column(String(5))
+
+    # 来源网站
+    source = Column(String(50))
+
+    # 代理实际提供者
+    supplier = Column(String(50))
+
+    # 代理类型
+    proxy_type = Column(Integer())
+    proxy_cover = Column(String(50))
+    check_count = Column(String(50))
+    region = Column(String(50))
 
     def __init__(self, ip, port):
         self.ip = ip
---
+++ spider/proxy/src/bean/proxy_type_enum.py
@@ -0,0 +1,11 @@
+# Coding : utf-8
+# Author : chyh
+# Date   : 2021/4/2 18:33
+
+
+# @unique
+# class ProxyTypeEnum(Enum):
+#     UNKNOWN = 0
+#     HTTP = 1
+#     HTTPS = 2
+#     HTTP_AND_HTTPS = 3
