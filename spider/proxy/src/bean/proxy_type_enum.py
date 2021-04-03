109271bd3989c079c42fc1a754ef17e49e0c8f9b
ppassion <chenyanhui1087@163.com>
2021-04-03 21:25:50 +0800

    proxy

--- spider/proxy/src/bean/proxy_type_enum.py
+++ spider/proxy/src/bean/proxy_type_enum.py
@@ -2,10 +2,12 @@
 # Author : chyh
 # Date   : 2021/4/2 18:33
 
+from enum import Enum, unique
 
-# @unique
-# class ProxyTypeEnum(Enum):
-#     UNKNOWN = 0
-#     HTTP = 1
-#     HTTPS = 2
-#     HTTP_AND_HTTPS = 3
+
+@unique
+class proxy_type_enum(Enum):
+    UNKNOWN = 0
+    HTTP = 1
+    HTTPS = 2
+    HTTP_AND_HTTPS = 3
